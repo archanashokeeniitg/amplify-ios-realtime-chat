5f07f218ecfb8dcf487979dfae0d8b696de889c6
archanashokeeniitg <archanashokeeniitg@gmail.com>
2021-05-16 14:29:29 -0700

    Initial Commit

---
+++ amplify-realtime-ios-chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  amplify-realtime-ios-chat
+//
+//  Created by Archana Shokeen on 5/16/21.
+//  Copyright © 2021 Archana Shokeen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
